4fd4e24f83f63926c6d2b371fb1f090d8c911031
KRANTHI MANCHIKANTI <ogkranthi22@gmail.com>
2018-04-19 17:10:40 -0400

    Update cloud URL for cors

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ function searchBattle(bat) {
 }
 
 var app = express();
-app.use(cors({ origin: 'http://0.0.0.0:49160' }));
+app.use(cors({ origin: 'http://129.156.117.66:9000' }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
